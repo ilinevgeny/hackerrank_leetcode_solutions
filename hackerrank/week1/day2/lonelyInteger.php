1491139a1f5a6981d10ccfa834a98a9ec9e7b523
Evgeny Ilin <e.ilin@dynatech.lv>
2025-08-07 23:13:28 +0300

    (task): implement lonely integer function

---
+++ hackerrank/week1/day2/lonelyInteger.php
@@ -0,0 +1,21 @@
+<?php
+
+/*
+ * Complete the 'lonelyinteger' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts INTEGER_ARRAY a as parameter.
+ */
+
+function lonelyinteger($a) {
+    $found = [];
+    for($i = 0, $iMax = count($a); $i < $iMax; $i++) {
+        $key = array_search($a[$i], $found);
+        if($key === false) {
+            $found[] = $a[$i];
+        } else {
+            unset($found[$key]);
+        }
+    }
+    return array_shift($found);
+}
